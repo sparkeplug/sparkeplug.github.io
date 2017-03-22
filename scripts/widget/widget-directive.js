bd7217469fe1636619d89f27d696f3d43ff5b14f
Prabhu Ram Kumar <prabhu.rk90@gmail.com>
2017-03-22 13:18:51 +0530

    Delete functionality implemented. New icons introduced. Pin functionality in progress

--- scripts/widget/widget-directive.js
+++ scripts/widget/widget-directive.js
@@ -3,13 +3,28 @@
 
   angular.module("prowarenessApp")
     .controller("widgetController",["$scope","widgetService",function($scope,widgetService){
-
+          $scope.pinnedItems = 0;
           widgetService.getWidgetData().then(function(response){
             $scope.widgetData = response.data;
             console.log($scope.widgetData);
           },function(error){
 
           });
+
+          $scope.delete = function(data){
+            $scope.widgetData.forEach(function(item, index, object){
+              if(item.name === data.name){
+                object.splice(index, 1);
+                exit();
+              }
+            });
+          };
+
+          $scope.pin = function(data){
+              $scope.pinnedItems+=1;
+              data.data.pinNumber = $scope.pinnedItems;
+              console.log(data.data);
+          };
     }])
     .directive("widget",function(){
       return {
